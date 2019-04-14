e24810bcc4416183c753b0977e3a414bd467f3b5
Faisal Qureshi <faisal1899@gmail.com>
2019-04-14 20:31:49 +0530

    feat(core): complete list/add/edit streams

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { pick } from 'lodash';
 
-const StreamEdit = () => {
-  return (
-    <div>Stream Edit</div>
-  );
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamEdit extends React.Component {
+  componentWillMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={pick(this.props.stream, ['title', 'description'])}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import { omit, mapKeys } from 'lodash';
+
+import {
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from '../actions/types';
+
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ...mapKeys(action.payload, 'id') };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return omit(state, action.payload);
+    default:
+      return state;
+  }
+}
